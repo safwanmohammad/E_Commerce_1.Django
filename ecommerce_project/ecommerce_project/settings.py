08d7411f39d0a0a15554ebbe6b8e5bb4d15305fe
MOHAMMED SAFWAN <110681039+safwanmohammad@users.noreply.github.com>
2023-06-08 03:19:04 +0530

    DATABASE CHANGE

--- ecommerce_project/ecommerce_project/settings.py
+++ ecommerce_project/ecommerce_project/settings.py
@@ -80,8 +80,10 @@ WSGI_APPLICATION = 'ecommerce_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecommerce_1',
+        'USER':'root',
+        'PASSWORD':'@Cjack004' 
     }
 }
 
